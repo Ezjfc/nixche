4609925998830a4f7816b7a86de6386e431ba083
Endermanbugzjfc <175898536+Ezjfc@users.noreply.github.com>
2026-02-23 00:33:22 +1100

    Fix(neovim-with-lsps): infinite recursion and some minor mistakes.

--- neovim/neovim-with-lsps/default.nix
+++ neovim/neovim-with-lsps/default.nix
@@ -3,67 +3,64 @@
 #
 # Note: this overlay is created for NvChad but should work for general Neovim
 # setups with some small changes. More passthru functions might be needed when
-# the current is not enought to suit my needs, like "withLspsAndConfigs"...
+# the current is not enough to suit my needs, like "withLspsAndConfigs"...
 #
 # References:
 # - https://github.com/NixOS/nixpkgs/blob/19eeef59bc3f75b5a9ffa17ab86a3d9512d505e5/pkgs/by-name/pu/pulumi/with-packages.nix
-
-final: prev: let
-  inherit (prev) neovim
-                 lib
-                 writeText
-                 runCommand
-                 makeBinaryWrapper;
-  needVersion = "0.11.0";
-  msgScope = "nixche/neovim/neovim-enable-lsps";
-
-  mkServerArgs = serverNames: lib.concatStringsSep " " (
-    builtins.map (serverName: let
-      enableScript = writeText "enable-${serverName}.lua" ''
-        vim.lsp.enable("${serverName}")
-      '';
-    in "-c \"source ${enableScript}\"" serverNames)
-  );
-  mkPrintArg = serverNames: let
-    serverNames' = lib.concatStringsSep ", " serverNames;
-    printScript = writeText "print-lsps.lua" ''
-      print("${msgScope}: enabling ${serverNames'}")
-    '';
-  in "-c \"source ${printScript}\"";
-  # Currently assume all language servers have meta.mainProgram.
-  mkServerLinks = serverPackages: lib.concatStrings (
-    builtins.map (serverPackage: let
-      program = serverPackage.meta.mainProgram;
-    in ''
-      ln -t "$out" "${serverPackage}/bin/${program}"
-    '') serverPackages
-  );
-
-  withLsps = servers: let
+final: prev: {
+  neovim = let
+    inherit (prev) neovim
+                   lib
+                   writeText
+                   runCommand
+                   makeWrapper;
+    needVersion = "0.11.0";
+    msgScope = "nixche/neovim/neovim-with-lsps";
     supportLsp = (builtins.compareVersions neovim.version needVersion) > -1;
 
-    serverNames = builtins.attrNames servers;
-    serverPackages = builtins.attrValues servers;
+    mkServerArgs = serverNames: lib.concatStringsSep " " (
+      builtins.map (serverName: let
+        enableScript = writeText "enable-${serverName}.lua" ''
+          vim.lsp.enable("${serverName}")
+        '';
+      in "-c \"source ${enableScript}\"") serverNames
+    );
+    mkPrintArg = serverNames: let
+      serverNames' = lib.concatStringsSep ", " serverNames;
+      printScript = writeText "print-lsps.lua" ''
+        print("${msgScope}: enabling ${serverNames'}")
+      '';
+    in "-c \"source ${printScript}\"";
+    # Currently assume all language servers have meta.mainProgram.
+    mkServerLinks = serverPackages: lib.concatStrings (
+      builtins.map (serverPackage: let
+        program = serverPackage.meta.mainProgram;
+      in ''
+        ln -s -t "$out" "${serverPackage}/bin/${program}"
+      '') serverPackages
+    );
 
-    nvimAndLsps = runCommand "neovim-with-lsps" {
-      buildInputs = serverPackages;
-      nativeBuildInputs = [ makeBinaryWrapper ];
-    } ''
-      mkdir -p "$out/bin"
-      ln -s -t "$out" "${neovim}/share"
-      makeWrapper "${neovim}/bin/nvim" "$out/bin/nvim"
+    passthru' = {
+      inherit withLsps;
+    };
+    withLsps = servers: let
+      serverNames = builtins.attrNames servers;
+      serverPackages = builtins.attrValues servers;
+      nvimAndLsps = runCommand "neovim-with-lsps" {
+        buildInputs = serverPackages;
+        nativeBuildInputs = [ makeWrapper ];
+      } ''
+        mkdir -p "$out/bin"
+        ln -s -t "$out" "${neovim}/share"
+        makeWrapper "${neovim}/bin/nvim" "$out/bin/nvim" \
+          --add-flags '${mkPrintArg serverNames} ${mkServerArgs serverNames}'
+        ${mkServerLinks serverPackages}
+      '';
+    in nvimAndLsps;
+  in assert lib.assertMsg supportLsp (
+    msgScope +
+    ": Neovim ${neovim.version} does not support Native LSP. " +
+    "Please upgrade to ${needVersion} or above"
+  ); neovim.overrideAttrs { passthru = (neovim.passthru or {}) // passthru'; };
+}
 
-      ${mkServerLinks serverPackages}
-    '';
-  in assert lib.assertMsg
-    (supportLsp)
-    (
-      msgScoe +
-      "Neovim ${neovim.version} does not support Native LSP. " +
-      "Please upgrade to ${needVersion} or above"
-    ) nvimAndLsps;
-  neovim' = neovim.overrideAttrs { passthru = (neovim.passthru or {}) // {
-    inherit withLsps;
-  }; };
-  overlay = { neovim = neovim'; };
-in overlay;
