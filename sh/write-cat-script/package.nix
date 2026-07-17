870ec6ca4813605e914a9454f8e471f71985aaae
EndermanbugZJFC <175898536+Ezjfc@users.noreply.github.com>
2026-07-18 01:05:35 +1000

    fix(write-cat-script-bin): wrong nesting

--- sh/write-cat-script/package.nix
+++ sh/write-cat-script/package.nix
@@ -1,1 +1,19 @@
-{}: import ./default.nix
+{
+  writeShellScript,
+  writeShellScriptBin,
+
+  colour ? "\\e[0;35m", # Dark purple.
+}: let
+  reset = "\\e[0m";
+  base = f: name: script: f name ''
+    echo -e "${colour}"
+    cat "$0" >&2
+    echo -e "${reset}"
+
+    ${script}
+  '';
+
+in {
+  writeCatScript = base writeShellScript;
+  writeCatScriptBin = base writeShellScriptBin;
+}
